3cf01c7a87f34e15a66631619fa8d67c92167361
embraun <65082266+embraun@users.noreply.github.com>
2020-05-09 19:02:05 +0200

    Update cachematrix.R
    Week 3 assignment, adding my script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+#making cached inverted matrix:
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#input matrix instead of vector
+makeCacheMatrix <- function(x = matrix()) { 
+    i <- NULL 
+    
+    #set matrix value
+    set <- function(y) { 
+        x <<- y
+        i <<- NULL
+    }
+    #get matrix value
+    get <- function() x
+    
+    #set the value of the invertible matrix
+    setinverse <- function(inverse) i <<- inverse
+    
+    #get the value of the invertible matrix
+    getinverse <- function() i
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#returning a matrix that is the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    #get value of inverted matrix from makeCacheMatrix function
+    i <- x$getinverse()
+    
+    #if inverse matrix is not NUll do that
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    
+    #if the value of the inversed matrix is NULL then do that
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    return(i)
 }
